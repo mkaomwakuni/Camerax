ceaadcc1cae40848dd4f769cc334244ace4bb93d
mkao_dev <mkaoibrahim@gmail.com>
2022-03-15 23:36:19 +0300

    second commit

--- app/src/main/java/com/mkao/camerax/MainActivity.kt
+++ app/src/main/java/com/mkao/camerax/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.mkao.camerax
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.pm.PackageManager
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.mkao.camerax.databinding.ActivityMainBinding
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +21,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -32,4 +37,28 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+    object CameraPermissionHelper {
+        private const val CAMERA_PERMISSION = Manifest.permission.CAMERA
+        private const val READ_PERMISSION = Manifest.permission.READ_EXTERNAL_STORAGE
+        fun hasCameraPermission(activity: Activity): Boolean {
+            return ContextCompat.checkSelfPermission(activity, CAMERA_PERMISSION)
+            == PackageManager.PERMISSION_GRANTED
+        }
+        fun hasStoragePermission(activity: Activity): Boolean {
+            return ContextCompat.checkSelfPermission(activity, READ_PERMISSION)
+            == PackageManager.PERMISSION_GRANTED
+        }
+        fun requestPermissions(activity: Activity) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
+                    CAMERA_PERMISSION)) {
+                AlertDialog.Builder(activity).apply {
+                    setMessage(activity.getString(R.string.permission_required))
+                    setPositiveButton(activity.getString(R.string.ok)) { _, _ ->
+                        ActivityCompat.requestPermissions(activity, arrayOf(CAMERA_PERMISSION,
+                            READ_PERMISSION), 1) }
+                    show() }
+            } else {
+                ActivityCompat.requestPermissions(activity, arrayOf(CAMERA_PERMISSION,
+                    READ_PERMISSION), 1) }
+        }}
 }
