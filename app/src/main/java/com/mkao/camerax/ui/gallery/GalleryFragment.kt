db3267c67f1759b0f5ee17bc7f86f496154af4b4
mkao_dev <mkaoibrahim@gmail.com>
2022-03-20 15:42:48 +0300

    Adapters and ViewModels fix

--- app/src/main/java/com/mkao/camerax/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/mkao/camerax/ui/gallery/GalleryFragment.kt
@@ -1,5 +1,6 @@
 package com.mkao.camerax.ui.gallery
 
+import GalleryViewModel
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,11 +8,13 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.mkao.camerax.MainActivity
 import com.mkao.camerax.databinding.FragmentGalleryBinding
 
 class GalleryFragment : Fragment() {
 
     private var _binding: FragmentGalleryBinding? = null
+    private lateinit var viewModel:GalleryViewModel
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,6 +28,21 @@ class GalleryFragment : Fragment() {
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View,savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // TODO: Apply the adapter to the RecyclerView
+        viewModel = ViewModelProvider (this)[GalleryViewModel::class.java]
+        viewModel.photos.observe(viewLifecycleOwner, {photos ->
+            photos?.let {
+            // TODO: Load the photo previews here }
+           }
+        })
+        if (MainActivity.CameraPermissionHelper.hasStoragePermission(requireActivity()
+            )) viewModel.loadPhotos()
+        else
+            MainActivity.CameraPermissionHelper.requestPermissions(requireActivity()) }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
